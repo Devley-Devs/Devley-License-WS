eecfb9c152ad03d06cd056e6e1e1ad424623dd9c
yuvaraja28 <yuv.the.dev@gmail.com>
2025-05-18 01:32:31 +0530

    Devley License WS Server V1.6

--- websocket.py
+++ websocket.py
@@ -1,10 +1,10 @@
-from typing import Union, Dict
 from json import JSONDecodeError
 import uvicorn, dotenv, os, httpx
 from collections import defaultdict
-from utils.utils import LicenseObject, ClientWSObject
+from typing import Union, Dict, Tuple
 from fastapi.responses import JSONResponse, PlainTextResponse
 from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
+from utils.utils import LicenseObject, ClientWSObject, ProductObject, UserObject
 
 dotenv.load_dotenv()
 PORT: int = int(os.getenv('PORT', 5000))
@@ -14,53 +14,99 @@ app = FastAPI()
 CONN_CLIENTS: Dict[str, Dict[str, ClientWSObject]] = defaultdict(dict)
 VERSION: float = float(os.getenv('VERSION', 1))
 DASHBOARD_URL: str = os.getenv('DASHBOARD_URL', '')
-SERVER_AUTH_CODE: str = os.getenv('SERVER_AUTH_CODE', '')
+
+def check_authorization(request: Request) -> Tuple[bool, Union[UserObject, None], Union[JSONResponse, None]]:
+    authorization: Union[str, None] = request.headers.get('Authorization')
+    if authorization is None:
+        return False, None, JSONResponse(
+            status_code=401,
+            content={ "error": "Authorization not Found" }
+        )
+    user_obj = UserObject(httpx.get(f"{DASHBOARD_URL}/api/account", headers={"Authorization": authorization}, timeout=10).json())
+    if user_obj._id is None:
+        return False, user_obj, JSONResponse(
+            status_code=401,
+            content={ "error": "Invalid Authentication" }
+        )
+    return True, user_obj, None
 
 @app.get("/")
 async def license_welcome():
     return PlainTextResponse(f"Devley License Websocket System V{VERSION}")
 
-@app.get("/getActiveInstances")
+@app.get("/api/getActiveInstances")
 async def active_instances(request: Request):
-    authorization: Union[str, None] = request.headers.get('Authorization')
-    if authorization != SERVER_AUTH_CODE:
-        return JSONResponse(
-            status_code=401,
-            content={ "error": "Invalid Authorization" }
-        )
-    product_slug: Union[str, None] = request.query_params.get('product_slug')
-    if product_slug:
-        connected_client = {client: dict(client_obj) for client, client_obj in CONN_CLIENTS.get(product_slug, {}).items()}
-        return JSONResponse(
-            status_code=200,
-            content=connected_client
-        )
+    auth_status, user_obj, auth_response = check_authorization(request)
+    if not auth_status or user_obj == None:
+        return auth_response
+    user_id: Union[str, None] = request.query_params.get('user_id')
+    connected_client = []
+    for conn in CONN_CLIENTS.values():
+        for instance in conn.values():
+            if ((user_obj.role == 'owner') and (user_id == '*')) or (instance.user_id == user_id) or (instance.user_id == user_obj._id):
+                connected_client.append(dict(instance))
     return JSONResponse(
-        status_code=404,
-        content={ "error": "product_slug not found in query" }
+        status_code=200,
+        content=connected_client
     )
 
-@app.websocket("/ws/{product_slug}/{license_key}")
-async def license_websocket(websocket: WebSocket, product_slug: str, license_key: str):
+@app.post("/api/terminateInstances")
+async def terminate_instances(request: Request):
+    auth_status, user_obj, auth_response = check_authorization(request)
+    if not auth_status or user_obj == None:
+        return auth_response
+    request_body = await request.json()
+    product_slug: Union[str, None] = request_body.get('product_slug')
+    license_key: Union[str, None] = request_body.get('license_key')
+    message: Union[str, None] = request_body.get('message')
+    if (product_slug == None) or (license_key == None) or (message == None):
+        return JSONResponse(
+            status_code=403,
+            content={ "error": "JSON Body is not fully passed" }
+        )
+    license_obj = LicenseObject(license_key)
+    if ((user_obj.role != 'owner') and (license_obj.user_id != user_obj.id)):
+        return JSONResponse(
+            status_code=401,
+            content={ "error": "Invalid Authentication" }
+        )
+    client_key = f'{license_obj.transaction_id}_{license_obj.user_id}'
+    instance = CONN_CLIENTS.get(product_slug, {}).get(client_key)
+    if instance == None:
+        return JSONResponse(
+            status_code=404,
+            content={ "error": "The Request Instance not found in the connected list" }
+        )
+    print("[!] Terminating Client Connection", instance.ws, instance.product_slug, license_key)
+    await instance.ws.send_json({ "event": "terminate", "error": message or "Forcibly Terminated by the User via Dashboard" })
+    await instance.ws.close()
+    return JSONResponse(
+        status_code=200,
+        content={
+            "status": "success",
+            "success": True
+        }
+    )
+
+@app.websocket("/ws/{product_slug}/{product_version}/{license_key}")
+async def license_websocket(websocket: WebSocket, product_slug: str, product_version: float, license_key: str):
     await websocket.accept()
-    valid_license = httpx.post(f"{DASHBOARD_URL}/api/getLicense",
-                                json={ 'product_slug': product_slug, 'license_key': license_key },
-                                headers={"Authorization": SERVER_AUTH_CODE}
+    valid_license = httpx.post(f"{DASHBOARD_URL}/api/getLicense", headers={"origin": 'devley-ws'}, timeout=10,
+                                json={ 'product_slug': product_slug, 'license_key': license_key, 'version': product_version }
                             )
     if valid_license.status_code != 200:
         await websocket.send_json({ "error": "Invalid License Key Provided" })
         return await websocket.close()
     license_obj = LicenseObject(license_key)
-    valid_license_obj = valid_license.json()
-    await websocket.send_json(valid_license_obj)
+    product_obj = ProductObject(valid_license.json())
     client_key = f'{license_obj.transaction_id}_{license_obj.user_id}'
     last_client = CONN_CLIENTS.get(product_slug, {}).get(client_key)
     if last_client != None:
-        print("[!] Closing Existing Client Connection")
-        await last_client.ws.send_json({ "event": "kill", "error": "Another Product Session is using the same License Key" })
+        print("[!] Closing Existing Client Connection", last_client.ws, last_client.product_slug, license_key)
+        await last_client.ws.send_json({ "event": "terminate", "error": "Another Product Session is using the same License Key" })
         await last_client.ws.close()
     print("[+] Client Connected", websocket, product_slug, license_key)
-    ws_client = ClientWSObject(websocket)
+    ws_client = ClientWSObject(websocket, product_obj, product_version, license_obj)
     CONN_CLIENTS[product_slug][client_key] = ws_client
     try:
         while True:
@@ -74,10 +120,6 @@ async def license_websocket(websocket: WebSocket, product_slug: str, license_key
         if last_client != None and last_client.session_id == ws_client.session_id:
             del CONN_CLIENTS[product_slug][client_key]
         print("[-] Client Disconnected", product_slug, websocket)
-
-# TODO
-# "event": "license_update"
-# "event": "exit"
 
 if __name__ == "__main__":
     uvicorn.run("websocket:app", host="0.0.0.0", port=PORT, reload=True)
